0101ba0450ce8ff4881c731c2860c104a05a42cc
John Puddephatt <john@jdp.org.uk>
2021-06-01 17:27:44 +0100

    update analytics code

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,48 +1,52 @@
 // import Vue from 'vue';
-import Vue from 'vue/dist/vue.js';
+import Vue from "vue/dist/vue.js";
 
-import VueRouter from 'vue-router';
-import 'leaflet/dist/leaflet.css';
+import VueRouter from "vue-router";
+import "leaflet/dist/leaflet.css";
 import VueGtag from "vue-gtag";
 
 Vue.use(VueRouter);
 
-window.axios = require('axios');
+window.axios = require("axios");
 
-import App from './components/App.vue';
-import Category from './components/Category.vue';
-import Entry from './components/Entry.vue';
+import App from "./components/App.vue";
+import Category from "./components/Category.vue";
+import Entry from "./components/Entry.vue";
 
-import NotFound from './components/404.vue';
+import NotFound from "./components/404.vue";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: App,
-    name: 'app',
+    name: "app",
     children: [
       {
-        path: ':slug',
+        path: ":slug",
         component: Category,
         props: true,
-        name: 'category'
+        name: "category",
       },
-    ]
+    ],
   },
-  { path: '*', component: NotFound }
-]
+  { path: "*", component: NotFound },
+];
 
 const router = new VueRouter({
-  routes // short for `routes: routes`
-})
+  routes, // short for `routes: routes`
+});
 
-Vue.use(VueGtag, {
-  config: { id: 'G-7WSGEPWFEE' }
-}, router)
+Vue.use(
+  VueGtag,
+  {
+    config: { id: "G-7PPC9H61Y7" },
+  },
+  router
+);
 
 const app = new Vue({
-  router
-}).$mount('#app')
+  router,
+}).$mount("#app");
 //
 // const app = new Vue({
 //   router,
